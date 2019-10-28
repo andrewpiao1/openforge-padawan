aca63ae5719a019f29c2797bb40b5c344a22ff25
andrewpiao1 <andrew.piao1@gmail.com>
2019-10-28 15:19:35 -0400

    cypress: completed navigation w/ content load tests

--- cypress/integration/my-tests/mytests.spec.js
+++ cypress/integration/my-tests/mytests.spec.js
@@ -1,4 +1,4 @@
-describe("Initial page load", ()=>{
+xdescribe("Initial page load", ()=>{
   beforeEach(()=>{
     cy.visit('http://localhost:8100/')
   })
@@ -6,29 +6,65 @@ describe("Initial page load", ()=>{
     cy.url().should('include','/tabs/feed-tab')
   })
 
+  it("should have a title", ()=>{
+    cy.title().should('be', 'OpenForce Ionic Assignment')
+  })
+
   it('should initialize on feed tab', ()=>{
     cy.contains('Feed').should('be.visible');
   })
 
   it('should render ion tab bar',()=>{
-
     cy.get('ion-tab-bar').should('be.visible')
-
   })
 
-  it('should render query both ionic tab elements navigators', ()=>{
+  it('should query and render both ionic tab navigator ', ()=>{
 
     cy.get('ion-tab-button').should('be.visible')
       .and('have.length', 2)
+  })
 
+  it('should correctly reload page', ()=>{
+    cy.reload()
+    cy.url().should('include', 'tabs/feed-tab')
   })
 
 })
 
-
-
-
-
 describe("Navigation", ()=>{
+  beforeEach(()=>{
+    cy.visit('http://localhost:8100/')
+  })
+
+  it('should go feed tab and successfully load content', ()=>{
+    //navigate to feed-tab
+    cy.visit('http://localhost:8100/tabs/feed-tab/')
+    cy.get('ion-item').should('have.length.greaterThan', 10)
+
+    cy.url().should('include', 'tabs/feed-tab')
+  })
+
+  it('should navigate to User/Search tab and successfully load content', ()=>{
+    //navigate to feed-tab
+    cy.get('ion-tab-button[tab="users-tab"]').click();
+    cy.contains('User/Search').should('be.visible')
+
+    cy.url().should('include', 'tabs/users-tab')
+
+    //successfull load search bar
+    cy.get('ion-searchbar').should('be.visible')
+  })
+
+  it('should navigate back to Feed tab and successfully load content', ()=>{
+    //navigate to feed-tab
+    cy.get('ion-tab-button[tab="feed-tab"]').click();
+    cy.contains('Users Feed').should('be.visible')
+
+    cy.url().should('include', 'tabs/feed-tab')
+
+    //successfully loaded entries
+    cy.get('ion-item').should('have.length.greaterThan', 10)
+
+  })
 
 })
